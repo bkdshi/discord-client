3bf96f85af64c4b67a988862e08ac4e70214ceb7
yk0o5h2e4i <y.k0o5h2e4i@gmail.com>
2023-01-02 16:00:39 +0900

    add show messages

--- main.go
+++ main.go
@@ -5,9 +5,13 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+var conf_file string = "bot.conf"
+var base_url string = "https://discordapp.com/api"
 
 type Bot struct {
 	Name    string `json:"name"`
@@ -15,7 +19,34 @@ type Bot struct {
 	Channel string `json:"channel"`
 }
 
-func registerBot() {
+type Author struct {
+	Id       string `json:"id"`
+	Username string `json:"username"`
+}
+
+type Message struct {
+	Id        string `json:"id"`
+	Author    Author `json:"author"`
+	Content   string `json:"content"`
+	Timestamp string `json:"timestamp"`
+}
+
+func loadBot() (Bot, error) {
+	var bot Bot
+	f, err := os.Open(conf_file)
+	if err != nil {
+		return bot, err
+	}
+	defer f.Close()
+
+	decoder := json.NewDecoder(f)
+	if err := decoder.Decode(&bot); err != nil {
+		return bot, err
+	}
+	return bot, nil
+}
+
+func registerBot() Bot {
 	var bot Bot
 	fmt.Println("Start registaration")
 	fmt.Print("Enter Bot name: ")
@@ -28,7 +59,7 @@ func registerBot() {
 	bot_json, err := json.MarshalIndent(bot, "", "\t")
 	if err != nil {
 		fmt.Println(err)
-		return
+		return bot
 	}
 	fmt.Println(string(bot_json))
 
@@ -37,32 +68,20 @@ func registerBot() {
 	fmt.Scan(&confirm)
 
 	if confirm != "y" {
-		return
+		return bot
 	}
 
-	f, err := os.Create("bot.conf")
+	f, err := os.Create(conf_file)
 	_, err = f.Write(bot_json)
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer f.Close()
-
+	return bot
 }
 
-func sendMessage() {
-	f, err := os.Open("bot.conf")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer f.Close()
-
-	var bot Bot
-	decoder := json.NewDecoder(f)
-	if err := decoder.Decode(&bot); err != nil {
-		fmt.Println(err)
-	}
-
-	url := fmt.Sprintf("https://discordapp.com/api/channels/%v/messages", bot.Channel)
+func sendMessage(bot Bot) {
+	url := fmt.Sprintf("%v/channels/%v/messages", base_url, bot.Channel)
 
 	fmt.Print("Enter message: ")
 	var message string
@@ -87,17 +106,59 @@ func sendMessage() {
 	fmt.Println(res.Status)
 }
 
-func main() {
-	fmt.Println("Hello")
+func showMessages(bot Bot) {
+	url := fmt.Sprintf("%v/channels/%v/messages", base_url, bot.Channel)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	req.Header.Add("Authorization", fmt.Sprintf("Bot %v", bot.Token))
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+	//fmt.Println(string(body))
 
-	register_flag := flag.Bool("r", false, "register bot")
-	send_flag := flag.Bool("s", false, "send a message")
+	var messages []Message
+	json.Unmarshal(body, &messages)
+	//fmt.Println(messages)
+
+	messages_json, err := json.MarshalIndent(messages, "", "\t")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(messages_json))
+}
+
+func main() {
+	conf_file = "bot.conf"
+	var bot Bot
+	fmt.Println("Hello")
+	register_flag := flag.Bool("register", false, "register bot")
+	send_flag := flag.Bool("send", false, "send a message")
+	show_flag := flag.Bool("show", false, "show messages")
 
 	flag.Parse()
+
+	if !*register_flag {
+		loaded_bot, err := loadBot()
+		if err != nil {
+			fmt.Println(err)
+			fmt.Println("Please execute with -r option")
+			return
+		}
+		bot = loaded_bot
+	}
 
 	if *register_flag {
 		registerBot()
 	} else if *send_flag {
-		sendMessage()
+		sendMessage(bot)
+	} else if *show_flag {
+		showMessages(bot)
 	}
 }
